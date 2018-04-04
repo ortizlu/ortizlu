7c3e64e53eb172e02f39f9bba5547d9d0ff194c2
ortizlu <ortizlu@icloud.com>
2018-04-04 13:38:04 -0400

    "Hide Email from Bots"

--- js/main.js
+++ js/main.js
@@ -30,3 +30,12 @@ $(document).ready(function(){
       } // End if
     });
   });
+
+
+  $(function() {
+    $('a[href^="mailto:"]').each(function() {
+     this.href = this.href.replace('(at)', '@').replace(/\(dot\)/g, '.');
+     // Remove this line if you don't want to set the email address as link text:
+     this.innerHTML = this.href.replace('mailto:', '');
+    });
+   });
