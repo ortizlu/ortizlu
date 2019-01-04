5646690764a7570d02ec4630c1db4fd4f43f361e
Luis Ortiz <ortizlu@icloud.com>
2019-01-04 12:37:50 -0500

    added switch statement which cleaned up code quite nicely. Still more refactoring to do

--- main.js
+++ main.js
@@ -13,8 +13,15 @@ let ateballCard = document.querySelector('.ateball');
 let resume = document.querySelector('#toggle-card-6');
 let resumeCard = document.querySelector('.me');
 
-let cards = document.querySelectorAll('.card');
-let labels = document.querySelectorAll('label');
+let triggers = document.querySelectorAll('[data-cardid]');
+let cards = [
+  spaceDashCard,
+  stageBrightCard,
+  cheeYunCard,
+  wowISSCard,
+  ateballCard,
+  resumeCard
+];
 
 //collapses upon click
 const collapseNav = function() {
@@ -38,104 +45,58 @@ html.addEventListener('click', e => {
   }
 });
 
-for (let i = 0; i < cards.length; i++) {
-  cards[i].addEventListener('click', e => {
-    let cardName = e.target.classList[1];
-    console.log(cardName);
+for (let i = 0; i < triggers.length; i++) {
+  triggers[i].addEventListener('click', e => {
+    switch (e.target.dataset.cardid) {
+      case '1':
+        spaceDash.checked = true;
+        stageBrightCard.style.zIndex = '6';
+        cheeYunCard.style.zIndex = '5';
+        wowISSCard.style.zIndex = '4';
+        ateballCard.style.zIndex = '3';
+        resumeCard.style.zIndex = '2';
+        break;
+      case '2':
+        stageBright.checked = true;
+        cheeYunCard.style.zIndex = '6';
+        wowISSCard.style.zIndex = '5';
+        ateballCard.style.zIndex = '4';
+        resumeCard.style.zIndex = '3';
+        spaceDashCard.style.zIndex = '2';
+        break;
+      case '3':
+        cheeYun.checked = true;
+        wowISSCard.style.zIndex = '6';
+        ateballCard.style.zIndex = '5';
+        resumeCard.style.zIndex = '4';
+        spaceDashCard.style.zIndex = '3';
+        stageBrightCard.style.zIndex = '2';
+        break;
+      case '4':
+        wowISS.checked = true;
+        ateballCard.style.zIndex = '6';
+        resumeCard.style.zIndex = '5';
+        spaceDashCard.style.zIndex = '4';
+        stageBrightCard.style.zIndex = '3';
+        cheeYunCard.style.zIndex = '2';
+        break;
+      case '5':
+        ateball.checked = true;
+        resumeCard.style.zIndex = '6';
+        spaceDashCard.style.zIndex = '5';
+        stageBrightCard.style.zIndex = '4';
+        cheeYunCard.style.zIndex = '3';
+        wowISSCard.style.zIndex = '2';
+        break;
+      case '6':
+        resume.checked = true;
+        spaceDashCard.style.zIndex = '6';
+        stageBrightCard.style.zIndex = '5';
+        cheeYunCard.style.zIndex = '4';
+        wowISSCard.style.zIndex = '3';
+        ateballCard.style.zIndex = '2';
+        break;
+      default:
+    }
   });
 }
-
-for (let i = 0; i < labels.length; i++) {
-  labels[i].addEventListener('click', e => {
-    let cardID = e.target.getAttribute('for');
-    console.log(cardID);
-    console.log(e.target.dataset.cardid);
-  });
-}
-
-spaceDashCard.addEventListener('click', () => {
-  spaceDash.checked = true;
-  stageBrightCard.style.zIndex = '6';
-  wowISSCard.style.zIndex = '5';
-  cheeYunCard.style.zIndex = '4';
-  resumeCard.style.zIndex = '2';
-});
-
-spaceDash.addEventListener('change', () => {
-  stageBrightCard.style.zIndex = '6';
-});
-
-stageBrightCard.addEventListener('click', e => {
-  e.stopPropagation();
-  stageBright.checked = true;
-  cheeYunCard.style.zIndex = '6';
-  spaceDashCard.style.zIndex = '5';
-  stageBrightCard.style.zIndex = '4';
-  wowISSCard.style.zIndex = '3';
-  resumeCard.style.zIndex = '2';
-});
-
-stageBright.addEventListener('change', () => {
-  cheeYunCard.style.zIndex = '6';
-});
-
-cheeYunCard.addEventListener('click', e => {
-  e.stopPropagation();
-  cheeYun.checked = true;
-  wowISSCard.style.zIndex = '6';
-  spaceDashCard.style.zIndex = '5';
-  stageBrightCard.style.zIndex = '4';
-  cheeYunCard.style.zIndex = '3';
-  resumeCard.style.zIndex = '2';
-});
-
-cheeYun.addEventListener('change', () => {
-  wowISSCard.style.zIndex = '6';
-});
-
-wowISSCard.addEventListener('click', e => {
-  e.stopPropagation();
-  wowISS.checked = true;
-  ateballCard.style.zIndex = '6';
-  spaceDashCard.style.zIndex = '5';
-  stageBrightCard.style.zIndex = '4';
-  cheeYunCard.style.zIndex = '3';
-  wowISSCard.style.zIndex = '2';
-});
-
-wowISS.addEventListener('change', () => {
-  if (wowISS.checked) {
-    resumeCard.style.zIndex = '6';
-  }
-});
-
-ateballCard.addEventListener('click', e => {
-  e.stopPropagation();
-  resume.checked = true;
-  resumeCard.style.zIndex = '6';
-  spaceDashCard.style.zIndex = '5';
-  wowISSCard.style.zIndex = '4';
-  cheeYunCard.style.zIndex = '3';
-  stageBrightCard.style.zIndex = '2';
-});
-
-ateball.addEventListener('change', () => {
-  if (resume.checked) {
-    ateballCard.style.zIndex = '6';
-  }
-});
-
-resumeCard.addEventListener('click', e => {
-  e.stopPropagation();
-  resume.checked = true;
-  spaceDashCard.style.zIndex = '6';
-  wowISSCard.style.zIndex = '5';
-  cheeYunCard.style.zIndex = '4';
-  stageBrightCard.style.zIndex = '3';
-});
-
-resume.addEventListener('change', () => {
-  if (resume.checked) {
-    spaceDashCard.style.zIndex = '6';
-  }
-});
